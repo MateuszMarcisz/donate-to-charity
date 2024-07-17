4861d6e22db3933d1274c33501c636099277418d
MateuszMarcisz <mateusz.marciszm@gmail.com>
2024-07-17 10:00:02 +0200

    add Institution options to adminpanel

--- charity_donations/admin.py
+++ charity_donations/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from charity_donations.models import Institution
 
-# Register your models here.
+
+class InstitutionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'type_display')  # Customize the list display in the admin panel
+    list_filter = ('type', 'categories')  # Filtering by type and categories
+    search_fields = ('name', 'description')  # Add search functionality based on name and description
+
+    def type_display(self, obj):
+        return obj.get_type_display()
+    type_display.short_description = 'Type'
+
+
+admin.site.register(Institution, InstitutionAdmin)
